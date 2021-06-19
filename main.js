1c4ca67f0f7a173d901f8caeba66bc8bf13af3aa
Luise5678 <81134635+Luise5678@users.noreply.github.com>
2021-06-19 15:36:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(".navbar .nav-link ").click(function () {
+    console.log(this);
+    const target = $(this).attr("href")
+    console.log("移動目標", target);
+    const position = $(target).offset().top
+    console.log("座標", position)
+    $("html,body").stop().animate({
+        scrollTop: position
+    }, 1000);
+});
+
